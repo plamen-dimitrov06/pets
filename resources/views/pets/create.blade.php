c61334128a6210e5b7eaf6ab0101fe85d2140059
Plamen Dimitrov <plamen.dimitrov006@gmail.com>
2025-03-23 15:58:08 +0200

    add required attribute to create pet form

--- resources/views/pets/create.blade.php
+++ resources/views/pets/create.blade.php
@@ -6,13 +6,13 @@
         @csrf
 
         <label for="name">Pet name :</label><br>
-        <input type="text" name="name"><br>
+        <input type="text" name="name" required><br>
 
         <label for="description">Pet description :</label><br>
-        <input type="text" name="description"><br>
+        <input type="text" name="description" required><br>
 
         <label for="type">Type</label><br>
-        <select name="type">
+        <select name="type" required>
             @foreach ($types as $type)
                 <option value="{{ $type->type }}">{{ ucfirst($type->type) }}</option>
             @endforeach
